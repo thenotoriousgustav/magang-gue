101c696b80c8142f5f0907152b09c672edacf04e
thenotoriousgustav <rhezagustam@gmail.com>
2025-03-28 03:35:04 +0700

    chore: initialize project with Prettier and ESLint setup; add environment configuration and GitHub Actions workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,107 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import * as fs from "fs"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// https://github.com/francoismassart/eslint-plugin-tailwindcss/pull/381
+// import eslintPluginTailwindcss from "eslint-plugin-tailwindcss"
+import eslintPluginImport from "eslint-plugin-import"
+import eslintPluginNext from "@next/eslint-plugin-next"
+import eslintPluginStorybook from "eslint-plugin-storybook"
+import typescriptEslint from "typescript-eslint"
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const eslintIgnore = [
+  ".git/",
+  ".next/",
+  "node_modules/",
+  "dist/",
+  "build/",
+  "coverage/",
+  "*.min.js",
+  "*.config.js",
+  "*.d.ts",
+]
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const config = typescriptEslint.config(
+  {
+    ignores: eslintIgnore,
+  },
+  //  https://github.com/francoismassart/eslint-plugin-tailwindcss/pull/381
+  // ...eslintPluginTailwindcss.configs["flat/recommended"],
+  typescriptEslint.configs.recommended,
+  eslintPluginImport.flatConfigs.recommended,
+  {
+    plugins: {
+      "@next/next": eslintPluginNext,
+    },
+    rules: {
+      ...eslintPluginNext.configs.recommended.rules,
+      ...eslintPluginNext.configs["core-web-vitals"].rules,
+    },
+  },
+  {
+    settings: {
+      tailwindcss: {
+        callees: ["classnames", "clsx", "ctl", "cn", "cva"],
+      },
 
-export default eslintConfig;
+      "import/resolver": {
+        typescript: true,
+        node: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "sort-imports": [
+        "error",
+        {
+          ignoreCase: true,
+          ignoreDeclarationSort: true,
+        },
+      ],
+      "import/order": [
+        "warn",
+        {
+          groups: ["external", "builtin", "internal", "sibling", "parent", "index"],
+          pathGroups: [
+            ...getDirectoriesToSort().map((singleDir) => ({
+              pattern: `${singleDir}/**`,
+              group: "internal",
+            })),
+            {
+              pattern: "env",
+              group: "internal",
+            },
+            {
+              pattern: "theme",
+              group: "internal",
+            },
+            {
+              pattern: "public/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["internal"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  }
+)
+
+function getDirectoriesToSort() {
+  const ignoredSortingDirectories = [".git", ".next", ".vscode", "node_modules"]
+  return fs
+    .readdirSync(process.cwd())
+    .filter((file) => fs.statSync(process.cwd() + "/" + file).isDirectory())
+    .filter((f) => !ignoredSortingDirectories.includes(f))
+}
+
+export default config
